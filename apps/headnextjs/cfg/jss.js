0e5891db3b079d395cbd64983053f625a928944e
Jesper Balle <jba@asmblii.com>
2025-03-08 14:00:50 +0100

    Add basic nextjs head app

---
+++ apps/headnextjs/cfg/jss.js
@@ -0,0 +1,19 @@
+const config = {
+    sitecoreApiKey: process.env.SITECORE_API_KEY ?? 'missing',
+    sitecoreApiHost: process.env.SITECORE_API_HOST ?? 'https://xmcloud.localhost',
+    sitecoreEdgeUrl: process.env.SITECORE_EDGE_URL ?? 'https://xmcloud.localhost',
+    sitecoreEdgeContextId: process.env.SITECORE_EDGE_CONTEXT ?? '',
+    sitecoreSiteName: process.env.SITECORE_SITE_NAME ?? 'website',
+    publicUrl: process.env_PUBLIC_URL ?? 'http://localhost:5000/',
+    graphQLEndpointPath: process.env.GRAPH_QL_ENDPOINT_PATH ?? '/sitecore/api/graph/edge',
+    defaultLanguage: process.env.DEFAULT_LANGUAGE ?? 'en',
+    locales: ['en'],
+    graphQLEndpoint: process.env.GRAPH_QL_ENDPOINT ?? '',
+    searchCustomerKey: process.env.SEARCH_CUSTOMERKEY,
+    searchApiKey: process.env.SEARCH_APIKEY,
+    searchApiHost: process.env.SEARCH_APIHOST,
+    searchEnv: process.env.SEARCH_ENV ?? 'prodEu',
+  };
+  
+  module.exports = config;
+  
