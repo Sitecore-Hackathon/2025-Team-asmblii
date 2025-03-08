544df195b191313e9b8c2088f8ded21b3264816e
Jesper Balle <jba@asmblii.com>
2025-03-08 18:23:21 +0100

    Fixes

--- apps/headnextjs/src/lib/graphql-client-factory/index.ts
+++ apps/headnextjs/src/lib/graphql-client-factory/index.ts
@@ -4,4 +4,16 @@ import { createGraphQLClientFactory } from './create';
 // The GraphQLRequestClientFactory serves as the central hub for executing GraphQL requests within the application
 
 // Create a new instance on each import call
-export default createGraphQLClientFactory(config);
+
+const cfg = {
+    sitecoreApiKey: config.sitecoreApiKey,
+    sitecoreApiHost: config.sitecoreApiHost,
+    sitecoreSiteName: config.sitecoreSiteName,
+    graphQLEndpointPath: config.graphQLEndpointPath,
+    defaultLanguage: config.defaultLanguage,
+    graphQLEndpoint: config.graphQLEndpoint,
+    layoutServiceConfigurationName: config.layoutServiceConfigurationName,
+    publicUrl: config.publicUrl,
+}
+
+export default createGraphQLClientFactory(cfg);
